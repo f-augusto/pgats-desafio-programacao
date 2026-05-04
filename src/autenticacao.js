85b93642f25a0bf3b3321b96572e8f7a4b97dd53
Fernando Santos <f.augusto94@hotmail.com>
2026-05-04 17:32:05 -0300

    Implementando a função de login

--- src/autenticacao.js
+++ src/autenticacao.js
@@ -1,5 +1,32 @@
-const usuarios = [];
+const usuarios = [
+  {
+    id: 1,
+    nome: 'Fernando Santos',
+    email: 'fernando.santos@pgats.com',
+    senha: 'fsSenhaCerta123',
+    expirado: false
+  },
+  {
+    id: 2,
+    nome: 'João Augusto',
+    email: 'joao.augusto@pgats.com',
+    senha: 'jaSenhaCerta123',
+    expirado: true
+  }
+]
 
-export function fazerLogin(){
+export function fazerLogin(email, senha) {
+  if (!email || !senha) {
+    throw new Error('Email e senha são obrigatórios.')
+  }
 
+  for (let i=0; i < usuarios.length; i++) {
+    if (usuarios[i].email == email && usuarios[i].senha == senha) {
+      if (usuarios[i].expirado) {
+        return ('Renove suas credenciais.')
+      }
+      return ('Login realizado com sucesso.')
+    }
+  }
+  return ('Credenciais incorretas.')
 }
